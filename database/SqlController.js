eb3c0cf15c6cf098bd9f84bf61563022d247113d
Daniel Flores <danielflores125@gmail.com>
2022-01-02 20:36:53 -0800

    Updated folder structure

--- database/SqlController.js
+++
@@ -1,5 +0,0 @@
-var mysql = require('mysql')
-var faker = require('faker');
-const { config } = require("../bdpw.js");
-
-var connection = mysql.createConnection(config);
--- database/SqlDB.js
+++
@@ -1,4 +0,0 @@
-var mysql = require('mysql')
-const { config } = require("../bdpw.js");
-
-var connection = mysql.createConnection(config);
