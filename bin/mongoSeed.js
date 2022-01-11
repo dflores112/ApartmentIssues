c05811878e6afa228c82dec0c8858e669dfb5032
Daniel Flores <danielflores125@gmail.com>
2022-01-10 22:26:24 -0800

    Added get request by issue id

--- bin/mongoSeed.js
+++ bin/mongoSeed.js
@@ -1,21 +1,25 @@
-const temp = {
-  description: 'adfa',
-  status: false,
-  date: new Date(),
-  tenant: {
-    name: 'marcelo',
-    phone: '912-192-2949',
-    email: 'ad2234h@gmail',
-    unit: 244,
+/*
+
+{
+"description": "adfa",
+  "status": false,
+  "date": null,
+  "tenant": {
+    "name": "jimjordan",
+    "phone": "912-192-2949",
+    "email": "ad2234h@gmail",
+    "unit": 244
   },
-  worker: {
-    name: 'jim',
-    phone: '292-123-1356',
-    email: 'adfa.com',
+  "worker": {
+    "name": "jim",
+    "phone": "292-123-1356",
+    "email": "adfa.com"
   },
-  complex: {
-    name: 'llc branna',
-    phone: '230-123-3234',
-    email: 'adfl@ritio.com',
-  },
-};
+  "complex": {
+    "name": "llc branna",
+    "phone": "230-123-3234",
+    "email": "adfl@ritio.com"
+  }
+}
+
+*/
